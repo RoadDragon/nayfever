739b04f7d1d596efd8440edd28bb1239540fb2eb
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-20 17:47:55 -0400

    changed destroy to delete_all

--- nayfever/db/seeds.rb
+++ nayfever/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Restaurant.destroy
-Allergy.destroy
+Restaurant.delete_all
+Allergy.delete_all
 
 restaurants = [
   {
